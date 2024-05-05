f0095e1e443ffd833cf68c4aef0af8a9d3a96bb2
SmakleAst <SmakleLover@yandex.ru>
2024-05-05 19:02:09 +0300

    Delete note command implementation

--- Notes.Backend/Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
+++ Notes.Backend/Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
@@ -4,30 +4,26 @@ using Notes.Application.Common.Exceptions;
 using Notes.Application.Interfaces;
 using Notes.Domain;
 
-namespace Notes.Application.Notes.Commands.UpdateNote
+namespace Notes.Application.Notes.Commands.DeleteCommand
 {
-    public class UpdateNoteCommandHandler : IRequestHandler<UpdateNoteCommand, Unit>
+    public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand, Unit>
     {
         private readonly INotesDbContext _dbContext;
 
-        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+        public DeleteNoteCommandHandler(INotesDbContext dbContext) => 
             _dbContext = dbContext;
-
-        public async Task<Unit> Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+        
+        public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
         {
             var entity = await _dbContext.Notes
-                .FirstOrDefaultAsync(note => note.Id == request.Id, cancellationToken);
+                .FindAsync(new object[] { request.Id }, cancellationToken);
 
             if (entity == null || entity.UserId != request.UserId)
             {
                 throw new NotFoundException(nameof(Note), request.Id);
             }
 
-            entity.Details = request.Details;
-            entity.Title = request.Title;
-            entity.EditDate = DateTime.Now;
-
-            _dbContext.Notes.Update(entity);
+            _dbContext.Notes.Remove(entity);
             await _dbContext.SaveChangesAsync(cancellationToken);
 
             return Unit.Value;
