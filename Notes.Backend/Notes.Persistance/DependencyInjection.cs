555c6dc34f1f49926a69d1d74a4920551110bc09
SmakleAst <SmakleLover@yandex.ru>
2024-05-05 20:01:02 +0300

    Add dependency injection

---
+++ Notes.Backend/Notes.Persistance/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["MSSQL"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+            services.AddScoped(provider =>
+            {
+                var dbContext = provider.GetService<NotesDbContext>();
+                return dbContext == null ? throw new Exception("NotesDbContext not found in service provider.") : (INotesDbContext)dbContext;
+            });
+
+
+            return services;
+        }
+    }
+}
