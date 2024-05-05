d2e9614e29589b41f0386fb8caf902f5ad15412b
SmakleAst <SmakleLover@yandex.ru>
2024-05-05 21:45:59 +0300

    Add GetNoteListQuery validator

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryValidator : AbstractValidator<GetNoteListQuery>
+    {
+        public GetNoteListQueryValidator()
+        {
+            RuleFor(updateNoteCommand => updateNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
