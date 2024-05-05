c6d84b72c01b99314d23a807e85967b7430fede9
SmakleAst <SmakleLover@yandex.ru>
2024-05-05 21:42:52 +0300

    Add delete note validator

---
+++ Notes.Backend/Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    public class DeleteNoteCommandValidator : AbstractValidator<DeleteNoteCommand>
+    {
+        public DeleteNoteCommandValidator()
+        {
+            RuleFor(updateNoteCommand => updateNoteCommand.UserId).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
