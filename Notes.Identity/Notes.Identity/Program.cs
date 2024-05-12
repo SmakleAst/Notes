74cf2f403803cfd63224ea700bfe21f48c792a61
SmakleAst <SmakleLover@yandex.ru>
2024-05-12 22:48:32 +0300

    Identity server confiduration implementation

--- Notes.Identity/Notes.Identity/Program.cs
+++ Notes.Identity/Notes.Identity/Program.cs
@@ -1,33 +1,63 @@
 using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddIdentityServer()
-    .AddInMemoryApiResources(new List<ApiResource>())
-    .AddInMemoryIdentityResources(new List<IdentityResource>())
-    .AddInMemoryApiScopes(new List<ApiScope>())
-    .AddInMemoryClients(new List<Client>())
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
+
+var connectionString = builder.Configuration.GetConnectionString("MSSQL");
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occured while app initialization");
+    }
 }
 
 app.UseIdentityServer();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
